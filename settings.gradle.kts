1579690a24cb556e1c1ae1511afd30bcbe67174a
이은진 <eunjin.lee@wadiz.kr>
2023-10-13 15:00:51 +0900

    init

---
+++ settings.gradle.kts
@@ -0,0 +1,19 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "DroidKnights2023_Clone_App"
+include(":app")
+include(":feature:main")
+include(":core:designsystem")
